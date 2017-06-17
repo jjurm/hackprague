ba932ca3b50989954ae752576ab1623c9ea4b4c9
Juraj Mičko <micko.juraj64@gmail.com>
2017-06-17 10:50:20 +0200

    Initial commit

---
+++ app/src/main/java/com/treecio/android/hackprague17/MainActivity.java
@@ -0,0 +1,13 @@
+package com.treecio.android.hackprague17;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
