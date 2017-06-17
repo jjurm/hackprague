9ff567c19b1736ab2104bcfde39f49a4f351e72a
Juraj Mičko <micko.juraj64@gmail.com>
2017-06-17 18:21:36 +0200

    Assing Calls to IDs

--- app/src/main/java/com/treecio/android/hackprague17/storage/StoredData.java
+++ app/src/main/java/com/treecio/android/hackprague17/storage/StoredData.java
@@ -3,7 +3,10 @@ package com.treecio.android.hackprague17.storage;
 import com.treecio.android.hackprague17.model.Call;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Object holding a bundle of data including call history.
@@ -11,14 +14,19 @@ import java.util.List;
 
 public class StoredData {
 
-    private List<Call> calls = new ArrayList<>();
+    private HashMap<Integer, Call> calls = new HashMap<>();
+    private AtomicInteger nextInt = new AtomicInteger(1);
 
     private StoredData() {
 
     }
 
-    public List<Call> getCalls() {
+    public HashMap<Integer, Call> getCalls() {
         return calls;
+    }
+
+    public Call getCall(int id) {
+        return calls.get(id);
     }
 
     public static StoredData newEmptyStoredData() {
