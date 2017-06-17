ebfcd9203e7373f9b280a597473b1c8ba7a7d835
padr31 <pali31pd@centrum.sk>
2017-06-17 22:07:25 +0200

    Continue CallActionAdapter

--- app/src/main/java/com/treecio/android/hackprague17/call/CallActionsAdapter.java
+++ app/src/main/java/com/treecio/android/hackprague17/call/CallActionsAdapter.java
@@ -2,6 +2,7 @@ package com.treecio.android.hackprague17.call;
 
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -9,6 +10,12 @@ import android.widget.TextView;
 
 import com.treecio.android.hackprague17.R;
 import com.treecio.android.hackprague17.model.Call;
+import com.treecio.android.hackprague17.model.CallAction;
+import com.treecio.android.hackprague17.model.CallAction.CallActionType;
+
+import java.util.List;
+
+import static com.treecio.android.hackprague17.model.CallAction.CallActionType.*;
 
 /**
  * Created by Pali on 17.06.2017.
@@ -20,17 +27,29 @@ public class CallActionsAdapter extends RecyclerView.Adapter<CallActionsAdapter.
 
     public CallActionsAdapter(Call call) {
         this.call = call;
-        //TODO get call actions
     }
 
     @Override
     public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View cardView = LayoutInflater.from(parent.getContext()).inflate(R.layout.action_card, parent, false);
+        return new CardViewHolder(cardView);
     }
 
     @Override
     public void onBindViewHolder(CardViewHolder holder, int position) {
 
+        List<CallAction> actions = call.getCallActions();
+        switch (actions.get(position).getType()) {
+            case Address:
+
+                break;
+            case Meet:
+                break;
+            case Remind:
+                break;
+            case Log:
+                break;
+        }
     }
 
     @Override
--- app/src/main/java/com/treecio/android/hackprague17/model/CallAction.java
+++ app/src/main/java/com/treecio/android/hackprague17/model/CallAction.java
@@ -14,7 +14,13 @@ public class CallAction {
 
     HashMap<String, String> parameters;
 
-    enum CallActionType {
+    public CallActionType getType() { return type; }
+
+    public String getDescription() { return description; }
+
+    public HashMap<String, String> getParameters() { return parameters; }
+
+    public static enum CallActionType {
         Remind,
         Meet,
         Address,
